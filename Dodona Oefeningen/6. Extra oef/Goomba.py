7e2f117a22b2f5a336977a3403be7b72a6eb6a06
@certiman <maarten.duhoux@gmail.com>
2023-01-19 15:03:55 +0100

    Update Goomba.py

--- Dodona Oefeningen/6. Extra oef/Goomba.py
+++ Dodona Oefeningen/6. Extra oef/Goomba.py
@@ -1,14 +1,12 @@
 #OPNIEUW GEMAAKT DOOR MIJ EN MIJN VADER
-#NIET GEBRUIKEN DAN WETEN ZE DIRECT DAT JE KOPIEERD
-
-
+#NIET GEBRUIKEN DAN WETEN ZE DIRECT DAT JE KOPIEERDE
 
 # werkt wel
-LK = int(input("X"))
-A = int(input("A"))
-B = int(input("B"))
-TotaleAfstand = (2 * LK - A) #enige berekening
-Snelheid = 1
-Tijd = int(TotaleAfstand / Snelheid)
+LK = int(input("Lengte van de kamer:"))
+A = int(input("Afstand A:"))
+B = int(input("Afstand B:"))
+TotaleAfstand = (2 * LK - A) #enige berekening, Eenheid is meter
+Snelheid = 1 # meter/minuut
+Tijd = int(TotaleAfstand / Snelheid) # dus in minuten
 
 print("De laatste Goomba heeft na", Tijd, "minuten de kamer verlaten")
